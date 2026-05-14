f36edf4a8e2d1ba35f4711b4ee803076bc705afc
Waterglass <82437210+applejin0105@users.noreply.github.com>
2026-05-15 00:15:46 +0900

    Add files via upload

---
+++ Scripts/Core/Attributes/Drawer/MinMaxSliderAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Core.Attributes.Drawer
+{
+    public class MinMaxSliderAttribute : PropertyAttribute
+    {
+        public MinMaxSliderAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+    }
+}
--- Scripts/Core/Attributes/SerializableDictionary.cs
+++ Scripts/Core/Attributes/SerializableDictionary.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Core.Attributes
+namespace Core.Attributes.Drawer
 {
     [Serializable]
     public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
